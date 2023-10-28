3fac8704cfbc6d43fe31c1ece1ddd9b58875c7fe
akrez <akrez.like@gmail.com>
2023-10-28 18:11:44 +0330

    updateBalance

--- app/Listeners/PaymentTransactionListener.php
+++ app/Listeners/PaymentTransactionListener.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Mail\PaymentApprovedMail;
 use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
@@ -23,17 +24,21 @@ class PaymentTransactionListener implements ShouldQueue
      */
     public function handle(object $event): void
     {
-        $balance = Transaction::where('user_id', $event->payment->user_id)
+        $user = User::findOrFail($event->payment->user_id);
+
+        $balance = Transaction::where('user_id', $user->id)
             ->where('currency_key', $event->payment->currency_key)
             ->sum('amount');
         $balance += $event->payment->amount;
 
-        $transaction = new Transaction();
-        $transaction->amount = $event->payment->amount;
-        $transaction->payment_id = $event->payment->id;
-        $transaction->balance = $balance;
-        $transaction->currency_key = $event->payment->currency->key;
-        $transaction->user_id = $event->payment->user_id;
-        $transaction->save();
+        Transaction::create([
+            'amount' => $event->payment->amount,
+            'payment_id' => $event->payment->id,
+            'balance' => $balance,
+            'currency_key' => $event->payment->currency->key,
+            'user_id' => $user->id,
+        ]);
+
+        $user->updateBalance();
     }
 }
