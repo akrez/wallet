afd88ea2df3e3e6b03416c940e79c868630397c6
akrez <akrez.like@gmail.com>
2023-10-28 18:11:25 +0330

    is_active

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Currency extends Model
 {
@@ -14,4 +15,13 @@ class Currency extends Model
     protected $casts = [
         'is_active' => 'boolean'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('is_active', function (Builder $builder) {
+            $builder->where('is_active', true);
+        });
+    }
 }
