a8c2616cbf63419a1686622b082b1432bb869b85
akrez <akrez.like@gmail.com>
2023-10-30 13:37:56 +0330

    FetchNavasanRatesJob

--- app/Console/Commands/FetchNavasanRatesCommand.php
+++ app/Console/Commands/FetchNavasanRatesCommand.php
@@ -2,12 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Exceptions\BadRequestException;
-use App\Exceptions\NotFoundException;
-use App\Models\Currency;
-use App\Models\Rate;
+use App\Jobs\FetchNavasanRatesJob;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 
 class FetchNavasanRatesCommand extends Command
 {
@@ -30,27 +26,6 @@ class FetchNavasanRatesCommand extends Command
      */
     public function handle()
     {
-        $currencyKeyToIsoCodes = Currency::whereNotIn('iso_code', ['irr'])->pluck('iso_code', 'key')->toArray();
-        if (empty($currencyKeyToIsoCodes)) {
-            throw new NotFoundException(__('currency.errors.currency_iso_code_is_empty'));
-        }
-
-        $navasanResponse = Http::get(config('settings.navasan.base_url'), [
-            'api_key' => config('settings.navasan.api_key'),
-        ]);
-        if (!$navasanResponse->ok()) {
-            throw new NotFoundException(__('currency.errors.currency_iso_code_is_empty'));
-        }
-
-        foreach ($currencyKeyToIsoCodes as $currencyKey => $currencyIsoCode) {
-            $navasanItem = $navasanResponse->json($currencyIsoCode);
-            if ($navasanItem) {
-                Rate::create([
-                    'currency_key' => $currencyKey,
-                    'rate' => $navasanItem['value'],
-                    'rate_currency_key' => 'rial',
-                ]);
-            }
-        }
+        FetchNavasanRatesJob::dispatch();
     }
 }
