765baddb1809a4040bd09f0f3dc9ff741ff5e919
akrez <akrez.like@gmail.com>
2023-10-31 14:28:17 +0330

    ExceptionHandler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,14 @@
 namespace App\Exceptions;
 
 use App\Facades\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidationException as ValidationValidationException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sentry\Laravel\Integration;
 use Throwable;
 
@@ -39,13 +46,49 @@ class Handler extends ExceptionHandler
         });
     }
 
-    public function render($request, Throwable $e)
+    public function render($request, Throwable $exception)
     {
-        if (in_array(get_class($e), $this->customExceptions)) {
-            return $e->render();
-        } else {
-            $statusCode = method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 500;
-            return Response::message($e->getMessage())->status($statusCode)->send();
+        if (in_array(get_class($exception), $this->customExceptions)) {
+            return $exception->render();
         }
+
+        if ($exception instanceof BadRequestException) {
+            return $this->renderError($exception, 400);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->renderError($exception, 404, __('error.url_not_found'));
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return $this->renderError($exception, 404, __('error.resource_not_found'));
+        }
+
+        if ($exception instanceof UnauthorizedException) {
+            return $this->renderError($exception, 401, __('auth.errors.an_authentication_error_occurred'));
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->renderError($exception, 403, __('errors.method_not_allowed'));
+        }
+
+        if ($exception instanceof ValidationValidationException) {
+            return $this->renderError($exception, 422, __('errors.the_sent_parameters_are_invalid'));
+        }
+
+        if ($exception instanceof ThrottleRequestsException) {
+            return $this->renderError($exception, 429, __('errors.too_many_request'));
+        }
+
+        return $this->renderError($exception, 500, __('errors.server_error'));
+    }
+
+    private function renderError(Throwable $exception, int $errorCode, ?string $message = null)
+    {
+        return response()->json([
+            'message' => ($message ? $message : $exception->getMessage()),
+            'errors' => (isset($exception->validator) ? $exception->validator->getMessageBag() : []),
+            'data' => []
+        ], $errorCode);
     }
 }
