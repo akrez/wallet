679dc0220b8bd91cb235f2df06e07d7209c6319d
akrez <akrez.like@gmail.com>
2023-10-31 13:51:23 +0330

    swagger auth

--- app/Contracts/Interfaces/Controllers/Controller.php
+++ app/Contracts/Interfaces/Controllers/Controller.php
@@ -5,8 +5,17 @@ namespace App\Contracts\Interfaces\Controllers;
 /**
  * @OA\Info(
  * version="1.0.0",
- * title="L5 OpenApi",
- * description="L5 Swagger OpenApi description"
+ * title="wallet doc",
+ * description="wallet doc"
+ * )
+ * @OA\SecurityScheme(
+ * type="http",
+ * description="Login with email and password to get the authentication token",
+ * name="Token based Based",
+ * in="header",
+ * scheme="bearer",
+ * bearerFormat="JWT",
+ * securityScheme="bearerAuth",
  * )
  **/
 interface Controller
